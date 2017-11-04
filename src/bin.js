e94735e48b4b4eb1e174e9a6fe7cd3ff87fb22c9
Zach Shilton <info@zachshilton.com>
2017-11-03 22:38:34 -0400

    Attempts to fix things so the package works as a command line tool.

---
+++ src/bin.js
@@ -0,0 +1,8 @@
+const convertFigmaSvgsForReact = require('./index.js')
+
+const args = process.argv.slice(2)
+console.log('Args...')
+console.log(JSON.stringify(process.argv, null, 2))
+const directory = args[0]
+
+convertFigmaSvgsForReact(directory)
